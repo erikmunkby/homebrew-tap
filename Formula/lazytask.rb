ae1fdfc3dc942cae33925cbd37cdc4e6f4123715
Erik Munkby <erik.munkby@gmail.com>
2026-03-10 19:06:59 +0100

    feat: add lazytask formula for v0.3.0

---
+++ Formula/lazytask.rb
@@ -0,0 +1,34 @@
+class Lazytask < Formula
+  desc "A task manager built for AI coding agents"
+  homepage "https://github.com/erikmunkby/lazytask"
+  license "MIT"
+  version "0.3.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/erikmunkby/lazytask/releases/download/lazytask-v#{version}/lt-aarch64-apple-darwin-lazytask-v#{version}.tar.gz"
+      sha256 "d3c3ee63411ac2339fa9f18806b63d6fd61cffadac692c24ae16093bc41d9f90"
+    else
+      url "https://github.com/erikmunkby/lazytask/releases/download/lazytask-v#{version}/lt-x86_64-apple-darwin-lazytask-v#{version}.tar.gz"
+      sha256 "53ec958a40dbf190fa88d12e45a8c22a4c1b3d1e7ffc9e851bf2211663381225"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      odie "lazytask is not yet available for Linux ARM"
+    else
+      url "https://github.com/erikmunkby/lazytask/releases/download/lazytask-v#{version}/lt-x86_64-unknown-linux-gnu-lazytask-v#{version}.tar.gz"
+      sha256 "eabd3add71af245364f023dbeb42ff6003b74285ff40edce3d310b29ac38a089"
+    end
+  end
+
+  def install
+    bin.install "lt"
+    bin.install_symlink "lt" => "lazytask"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/lt --version")
+  end
+end
